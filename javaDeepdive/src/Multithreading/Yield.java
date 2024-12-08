e9df0b50d114b95a00a7e3b48e75f0bb8380fedb
DonMB77 <purposefuldrifter@gmail.com>
2024-12-08 19:14:02 +0100

    yield() sandboxing

--- javaDeepdive/src/Multithreading/Yield.java
+++ javaDeepdive/src/Multithreading/Yield.java
@@ -2,5 +2,16 @@ package Multithreading;
 
 public class Yield {
 
-    
+    public static void main (String[] args) {
+        var thread0 = new Thread(() -> {
+            //gives hint to scheduler that the process is ready to yield its place in the processor
+            Thread.yield();
+            System.out.println(Thread.currentThread().getName());
+        });
+
+        var thread1 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+
+        thread0.start();
+        thread1.start();
+    }
 }
