729fabf8089dea8146b4653dac53cc80fcea25d4
DonMB77 <purposefuldrifter@gmail.com>
2024-12-08 15:43:13 +0100

    Implementation DefaultRunnable

--- javaDeepdive/src/Multithreating/FirstProgram.java
+++ javaDeepdive/src/Multithreating/FirstProgram.java
@@ -1,4 +1,27 @@
 package Multithreating;
 
 public class FirstProgram {
+
+    public static void main (String[] args) {
+
+        Runnable task = new DefaultRunnable();
+    }
+}
+
+class DefaultRunnable implements Runnable {
+
+    private String extString;
+
+    public DefaultRunnable() {
+
+    }
+
+    public DefaultRunnable(String extString) {
+        this.extString = extString;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("This is a new thread. My name is " + Thread.currentThread().getName());
+    }
 }
