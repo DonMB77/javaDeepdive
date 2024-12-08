cddb2a13b49d3b943cf98b14bb42a5a1659b13b5
DonMB77 <purposefuldrifter@gmail.com>
2024-12-08 16:09:57 +0100

    run() and start() difference and Thread invocation

--- javaDeepdive/src/Multithreating/FirstProgram.java
+++ javaDeepdive/src/Multithreating/FirstProgram.java
@@ -1,10 +1,48 @@
 package Multithreating;
 
-public class FirstProgram {
+public class FirstProgram extends Thread{
 
     public static void main (String[] args) {
 
         Runnable task = new DefaultRunnable();
+        Thread thread1 = new Thread(task);
+
+        // you can also use an anonymous class. This however is not very popular.
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("This is a new thread. My name is " + Thread.currentThread().getName());
+            }
+        });
+
+        // This is often referenced in literature. But not the best approach due to single responsibility principle.
+        Thread thread3 = new FirstProgram();
+
+        // Here we use Lambda-Expression to implement run()
+        Thread thread4 = new Thread(() -> System.out.println("This is a new thread. My name is " + Thread.currentThread().getName()));
+
+        Thread thread5 = new Thread(FirstProgram::execute);
+
+        System.out.println("This is a new thread. My name is " + Thread.currentThread().getName());
+        thread1.run(); // This doesn't start the thread. Only start() does that. There might not be a lot of sense in just calling run().
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+    }
+
+
+    // here we override the run method of Runnable. This makes us able to create a new thread as seen with thread3
+    @Override
+    public void run() {
+        System.out.println("This is a new thread. My name is " + Thread.currentThread().getName());
+    }
+
+    // meets the definition of runnable interface. It doesn't take any arguments and returns nothing.
+    public static void execute(){
+        System.out.println("This is a new thread. My name is " + Thread.currentThread().getName());
     }
 }
 
@@ -23,5 +61,6 @@ class DefaultRunnable implements Runnable {
     @Override
     public void run() {
         System.out.println("This is a new thread. My name is " + Thread.currentThread().getName());
+        // of course fields of the class can be used here as well
     }
 }
